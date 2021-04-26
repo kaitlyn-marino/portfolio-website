c5d048867db34d77f3713ee313e5a362178237a8
kaitlyn_m <kaitlyn.a.marino@gmail.com>
2021-04-26 12:23:03 -0400

    Responsive design, clients page, projects

---
+++ dominion-national-website/dentists/index.php
@@ -0,0 +1,201 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Dominion National</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="../site.webmanifest">
+  <link rel="apple-touch-icon" href="../icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="../css/main.css"> <!--Link to main stylesheet including header, nav, footer-->
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="../css/normalize.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <header> <!--Logo, Search, Navigation Menu & Hero Image-->
+    <?php include('../header.php'); ?>
+  </header>
+
+  <main>
+
+    <div class="middle-line"></div>
+
+    <section id="about">
+      <h1>Six Ways Dominion's Plans Work For you!</h1>
+      <h2>We remain committed to delivering exceptional service to our dentists. Our website is designed to help you learn more about how Dominion's plans work for you.</h2>
+    </section>
+
+    <section id="blocks">
+
+      <div class="block-one">
+        <div class="header">
+          <div class="image-width">
+            <img src="img/puzzle.png" alt="Icon" />
+          </div>
+          <div class="header-width">
+            <h3>BUILDING BUSINESS</h3>
+          </div>
+          <p>Dominion delivers dental benefits to over 900,000 members in the Mid-Atlantic region, and we aggressively promote our participating dentists with sophisticated search tools and online marketing. Our membership includes leading employer groups, municipalities, associations, individuals and health plans (including Capital BlueCross, Kaiser Foundation Health Plan of the Mid-Atlantic States, Inc. and UPMC).</p>
+        </div>
+        
+        <div class="yellow-line"></div>
+
+      </div>
+
+      <div class="block-two">
+        <div class="header">
+          <div class="image-width">
+            <img src="img/puzzle.png" alt="Icon" />
+          </div>
+          <div class="header-width">
+            <h3>CUSTOMIZED COMPENSATION</h3>
+          </div>
+          <p>Based on fee schedules designed specifically for your area, dentist compensation is routinely adjusted for increases in local dental fees and related costs in our Mid-Atlantic markets. Compare our fee schedules for yourself!</p>
+        </div>
+        
+        <div class="yellow-line"></div>
+
+      </div>
+
+      <div class="block-three">
+        <div class="header">
+          <div class="image-width">
+            <img src="img/puzzle.png" alt="Icon" />
+          </div>
+          <div class="header-width">
+            <h3>STREAMLINED SERVICES</h3>
+          </div>
+          <p>A secure Web portal for participating dentists gives you access to patient eligibility, plan information, treatment history and claim details. It also lets you submit claims electronically through your system free of charge as well as update your dental office information. Please visit <a href="https://www.dominionprovider.com/" target="_blank">DominionProvider.com</a> to log in today!</p>
+          <P>Preventive Care Only Dental Plans (Through Kaiser Permanente Mid-Atlantic): Fee schedules were recently increased.</p>
+        
+        <div class="button">
+          <a href="https://dominionprovider.com/" target="_blank">
+            <p>View Fee Schedule</p>
+          </a>
+        </div>
+      </div>
+        <div class="yellow-line"></div>
+
+      </div>
+    
+      <div class="block-four">
+        <div class="header">
+          <div class="image-width">
+            <img src="img/puzzle.png" alt="Icon" />
+          </div>
+          <div class="header-width">
+            <h3>POWERFUL PROCESSING</h3>
+          </div>
+          <p>With one of the most advanced claims processing systems in the industry, you get maximum quality control and simplified administration. Our approach reduces payment times, even with missing data or improperly filed forms. Check out these high standards:
+            <ul>
+              <li>Claim processing accuracy: 99% or better<sup>1</sup></li>
+              <li>Average clean claim processing: Within 6 business days<sup>2</sup></li>
+            </ul>
+          </p>
+        </div>
+        
+        <div class ="yellow-line"></div>
+
+      </div>
+
+      <div class="block-five">
+        <div class="header">
+          <div class="image-width">
+            <img src="img/puzzle.png" alt="Icon" />
+          </div>
+          <div class="header-width">
+            <h3>PRODUCTS & PLANS</h3>
+          </div>
+          <p>Dominion is one of the leading dental carriers in the Mid-Atlantic, providing a wide variety of innovative plans. We sell to a broad consumer market – continually increasing our membership and your patient base.
+            <ul>
+              <li>Traditional PPO Plans</li>
+              <li>Point-of-Service (POS) Plans</li>
+              <li>PPO Scheduled Plans</li>
+              <li>DHMO Plans (dentists must apply separately)</li>
+            </ul>
+          </p>
+        </div>
+        
+        <div class="yellow-line"></div>
+
+      </div>
+    
+      <div class="block-six">
+        <div class="header">
+          <div class="image-width">
+            <img src="img/puzzle.png" alt="Icon" />
+          </div>
+          <div class="header-width">
+            <h3>ALWAYS AVAILABLE</h3>
+          </div>
+          <p>We work hard to make your life easy: Toll-free and online service 24/7/365 with an Interactive Voice Response (IVR) system. To minimize administrative inquiries to your office, our groups and members are given password-protected online access to their accounts.</p>
+        </div>
+      </div>
+    
+    </section>
+
+    <section id="call-out">
+      <h3>Simple Sign Up</h3>
+      <div class="button2">
+        <div class="button">
+          <a href="https://www.dominionnational.com/wps/wcm/connect/prodnws.dominionnational.com-30097/4133eb1c-4b8c-4ec1-8261-0257c8bd0ded/Dental+Office+Profile+%28Provider+Application%29.pdf?MOD=AJPERES&CVID=na4Wvh7" target="_blank">
+            <p>Complete an Application</p>
+          </a>
+        </div>
+        <div class="button">
+          <a href="tel:888.471.3631">
+            <p>Speak to Provider Services</p>
+          </a>
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+  <footer>
+    <p>Privacy and Compliance Resources &#9679 Disclaimer &#9679 Careers &#9679 Dental FAQ &#9679 Contact Us &#9679 Transparency Claim Payment Policies &#9679 Nondiscrimination and Foreign Language Assistance Notice</p>
+    <p>251 18th Street South, Suite 900, Arlington, VA 22202</p>
+    <img src="../img/social-media.png" alt="Social Media Icons" />
+    <br>
+    <h1>Affiliations and Recognition</h1>
+    <img src="../img/affiliations.png" alt="Affiliations and Recognition Logos" />
+    <br>
+    <div class="goc">
+      <p>&#169 2020 Dominion National<br>
+        Dominion National is the brand name for the Dominion group of
+        companies. Dental plans are underwritten by Dominion Dental
+        Services, Inc. (DDSI). Dominion Dental Services USA, Inc.
+        (DDSUSA) is a licensed administrator of dental and vision benefits.
+        Vision plans are underwritten by Avalon Insurance Company, and
+        administered by DDSUSA, in DC, DE, MD, PA and VA. Vision 
+        Plans are underwritten by DDSI in all other states where 
+        Dominion National operates. The Discount Program is offered 
+        through DDSUSA.</p>
+    </div>
+  </footer>
+
+  <script src="../js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="../js/plugins.js"></script>
+  <script src="../js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
